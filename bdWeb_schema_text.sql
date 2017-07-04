477d8c64fd25ce8e39295bf40706d3d0fbd15671
fedeolivera1 <fedeolivera1@gmail.com>
2017-07-04 02:20:16 -0300

    cmt [04/07]
    Se modifica para eliminar sinc y ultact de lineas de pedido. Se agrega schema para base web.

--- bdWeb_schema_text.sql
+++ bdWeb_schema_text.sql
@@ -1,10 +1,6 @@
 drop table DEPARTAMENTO;
 
-drop table DEPOSITO;
-
 drop table LOCALIDAD;
-
-drop table LOTE;
 
 drop table PEDIDO;
 
@@ -22,58 +18,27 @@ drop table TIPO_DOC;
 
 drop table TIPO_PROD;
 
-drop table TRANSACCION;
-
-drop table TRAN_ESTADO;
-
-drop table TRAN_LINEA;
-
 drop table UNIDAD;
 
-drop table USR_DSK;
-
-drop table UTILIDAD;
+drop table USR_WEB;
 
 /*==============================================================*/
 /* Table: DEPARTAMENTO                                          */
 /*==============================================================*/
 create table DEPARTAMENTO (
-   ID_DEP               SERIAL not null,
+   ID_DEP               INTEGER              not null,
    NOMBRE               TEXT                 not null,
    constraint PK_DEPARTAMENTO primary key (ID_DEP)
-);
-
-/*==============================================================*/
-/* Table: DEPOSITO                                              */
-/*==============================================================*/
-create table DEPOSITO (
-   NRO_DEP              SERIAL               not null,
-   NOMBRE               TEXT                 not null,
-   constraint PK_DEPOSITO primary key (NRO_DEP)
 );
 
 /*==============================================================*/
 /* Table: LOCALIDAD                                             */
 /*==============================================================*/
 create table LOCALIDAD (
-   ID_LOC               SERIAL not null,
+   ID_LOC               INTEGER              not null,
    NOMBRE               TEXT                 not null,
    ID_DEP               INTEGER              null,
    constraint PK_LOCALIDAD primary key (ID_LOC)
-);
-
-/*==============================================================*/
-/* Table: LOTE                                                  */
-/*==============================================================*/
-create table LOTE (
-   ID_LOTE              SERIAL not null,
-   NRO_TRANSAC          INTEGER              not null,
-   ID_PRODUCTO          INTEGER              not null,
-   VENC                 DATE                 null,
-   NRO_DEP              INTEGER              null,
-   ID_UTIL              INTEGER              null,
-   STOCK                INTEGER              not null,
-   constraint PK_LOTE primary key (ID_LOTE)
 );
 
 /*==============================================================*/
@@ -82,17 +47,15 @@ create table LOTE (
 create table PEDIDO (
    ID_PERSONA           BIGINT               not null,
    FECHA_HORA           TIMESTAMP            not null,
-   ESTADO               CHAR                 not null,
+   ESTADO               CHAR(1)              not null,
    FECHA_PROG           DATE                 null,
    HORA_PROG            TIME                 null,
    ORIGEN               CHAR(1)              not null,
    SUB_TOTAL            NUMERIC(12,2)        not null,
-   IVA                  NUMERIC(12,2)        not null,
+   IVA                  DECIMAL(12,2)        null,
    TOTAL                NUMERIC(12,2)        not null,
-   NOM_USU              TEXT                 not null,
-   NRO_TRANSAC          INTEGER              null,
-   SINC                 CHAR(1)              null,
-   ULT_ACT              TIMESTAMP            null,
+   SINC                 CHAR(1)              not null,
+   ULT_ACT              TIMESTAMP            not null,
    constraint PK_PEDIDO primary key (ID_PERSONA, FECHA_HORA),
    constraint CKT_PEDIDO check (SINC in ('S', 'N'))
 );
@@ -105,9 +68,10 @@ create table PEDIDO_LINEA (
    FECHA_HORA           TIMESTAMP            not null,
    ID_PRODUCTO          INTEGER              not null,
    CANTIDAD             INTEGER              not null,
-   IVA                  DECIMAL(12,2)        null,
+   IVA                  NUMERIC(12,2)        null,
    PRECIO_UNIT          NUMERIC(12,2)        null,
-   constraint PK_PEDIDO_LINEA primary key (ID_PERSONA, FECHA_HORA, ID_PRODUCTO)
+   constraint PK_PEDIDO_LINEA primary key (ID_PERSONA, FECHA_HORA, ID_PRODUCTO),
+   constraint CKT_PEDIDO_LINEA check (SINC in ('S', 'N'))
 );
 
 /*==============================================================*/
@@ -118,14 +82,14 @@ create table PERSONA (
    DIRECCION            TEXT                 not null,
    PUERTA               TEXT                 null,
    SOLAR                TEXT                 null,
-   MANZANA              TEXT                 null,
+   MANZANA              INTEGER              null,
    KM                   NUMERIC(7,2)         null,
    COMPLEMENTO          TEXT                 null,
    TELEFONO             TEXT                 null,
    CELULAR              TEXT                 null,
    EMAIL                TEXT                 null,
    FECHA_REG            DATE                 null,
-   TIPO                 char(1)              not null,
+   TIPO                 char(1)              null,
    ID_LOC               INTEGER              null,
    ORIGEN               CHAR(1)              not null,
    SINC                 CHAR(1)              not null,
@@ -145,8 +109,9 @@ create table PERS_FISICA (
    NOMBRE2              TEXT                 null,
    FECHA_NAC            DATE                 null,
    SEXO                 char(1)              null,
+   NOM_USU              TEXT                 null,
    constraint PK_PERS_FISICA primary key (DOCUMENTO),
-   constraint CK_CLIENTE check (SEXO in ('M', 'F') AND SINC in ('S', 'N'))
+   constraint CKT_PERS_FISICA check (SINC in ('S', 'N') AND SEXO in ('M', 'F'))
 );
 
 /*==============================================================*/
@@ -186,7 +151,7 @@ create table PRODUCTO (
 /* Table: TIPO_DOC                                              */
 /*==============================================================*/
 create table TIPO_DOC (
-   ID_TIPO_DOC          SERIAL not null,
+   ID_TIPO_DOC          INTEGER              not null,
    NOMBRE               TEXT                 null,
    constraint PK_TIPO_DOC primary key (ID_TIPO_DOC)
 );
@@ -195,48 +160,9 @@ create table TIPO_DOC (
 /* Table: TIPO_PROD                                             */
 /*==============================================================*/
 create table TIPO_PROD (
-   ID_TIPO_PROD         SERIAL not null,
+   ID_TIPO_PROD         INTEGER              not null,
    DESCRIPCION          TEXT                 not null,
    constraint PK_TIPO_PROD primary key (ID_TIPO_PROD)
-);
-
-/*==============================================================*/
-/* Table: TRANSACCION                                           */
-/*==============================================================*/
-create table TRANSACCION (
-   NRO_TRANSAC          SERIAL not null,
-   ID_PERSONA           BIGINT               null,
-   OPERACION            CHAR(1)              not null,
-   FECHA_HORA           TIMESTAMP            null,
-   SUB_TOTAL            NUMERIC(12,2)        not null,
-   IVA                  NUMERIC(12,2)        not null,
-   TOTAL                NUMERIC(12,2)        not null,
-   ESTADO_ACT           CHAR(1)              null,
-   constraint PK_TRANSACCION primary key (NRO_TRANSAC),
-   constraint CKT_TRANSACCION check (OPERACION in ('V', 'C'))
-);
-
-/*==============================================================*/
-/* Table: TRAN_ESTADO                                           */
-/*==============================================================*/
-create table TRAN_ESTADO (
-   NRO_TRANSAC          INTEGER              not null,
-   ESTADO               CHAR(1)              not null,
-   FECHA_HORA           TIMESTAMP            not null,
-   constraint PK_TRAN_ESTADO primary key (NRO_TRANSAC, ESTADO),
-   constraint CKT_TRAN_ESTADO check (ESTADO in ('P', 'C', 'A'))
-);
-
-/*==============================================================*/
-/* Table: TRAN_LINEA                                            */
-/*==============================================================*/
-create table TRAN_LINEA (
-   NRO_TRANSAC          INTEGER              not null,
-   ID_PRODUCTO          INTEGER              not null,
-   CANTIDAD             INTEGER              not null,
-   IVA                  NUMERIC(12,2)        not null,
-   PRECIO_UNIT          NUMERIC(12,2)        not null,
-   constraint PK_TRAN_LINEA primary key (NRO_TRANSAC, ID_PRODUCTO)
 );
 
 /*==============================================================*/
@@ -249,53 +175,17 @@ create table UNIDAD (
 );
 
 /*==============================================================*/
-/* Table: USR_DSK                                               */
+/* Table: USR_WEB                                               */
 /*==============================================================*/
-create table USR_DSK (
+create table USR_WEB (
    NOM_USU              TEXT                 not null,
-   PASSWD               TEXT                 null,
-   TIPO                 CHAR(1)              not null,
-   constraint PK_USR_DSK primary key (NOM_USU)
-);
-
-/*==============================================================*/
-/* Table: UTILIDAD                                              */
-/*==============================================================*/
-create table UTILIDAD (
-   ID_UTIL              SERIAL not null,
-   DESCRIPCION          TEXT                 not null,
-   PORC                 DECIMAL(5,2)         not null,
-   constraint PK_UTILIDAD primary key (ID_UTIL)
+   PASSWD               TEXT                 not null,
+   constraint PK_USR_WEB primary key (NOM_USU)
 );
 
 alter table LOCALIDAD
    add constraint FK_LOCALIDA_REFERENCE_DEPARTAM foreign key (ID_DEP)
       references DEPARTAMENTO (ID_DEP)
-      on delete restrict on update restrict;
-
-alter table LOTE
-   add constraint FK_LOTE_REFERENCE_DEPOSITO foreign key (NRO_DEP)
-      references DEPOSITO (NRO_DEP)
-      on delete restrict on update restrict;
-
-alter table LOTE
-   add constraint FK_LOTE_REFERENCE_UTILIDAD foreign key (ID_UTIL)
-      references UTILIDAD (ID_UTIL)
-      on delete restrict on update restrict;
-
-alter table LOTE
-   add constraint FK_LOTE_REFERENCE_TRAN_LIN foreign key (NRO_TRANSAC, ID_PRODUCTO)
-      references TRAN_LINEA (NRO_TRANSAC, ID_PRODUCTO)
-      on delete restrict on update restrict;
-
-alter table PEDIDO
-   add constraint FK_PEDIDO_REFERENCE_USR_DSK foreign key (NOM_USU)
-      references USR_DSK (NOM_USU)
-      on delete restrict on update restrict;
-
-alter table PEDIDO
-   add constraint FK_PEDIDO_REFERENCE_TRANSACC foreign key (NRO_TRANSAC)
-      references TRANSACCION (NRO_TRANSAC)
       on delete restrict on update restrict;
 
 alter table PEDIDO
@@ -316,6 +206,11 @@ alter table PEDIDO_LINEA
 alter table PERSONA
    add constraint FK_PERSONA_REFERENCE_LOCALIDA foreign key (ID_LOC)
       references LOCALIDAD (ID_LOC)
+      on delete restrict on update restrict;
+
+alter table PERS_FISICA
+   add constraint FK_PERS_FIS_REFERENCE_USR_WEB foreign key (NOM_USU)
+      references USR_WEB (NOM_USU)
       on delete restrict on update restrict;
 
 alter table PERS_FISICA
@@ -341,24 +236,4 @@ alter table PRODUCTO
 alter table PRODUCTO
    add constraint FK_PRODUCTO_REFERENCE_TIPO_PRO foreign key (ID_TIPO_PROD)
       references TIPO_PROD (ID_TIPO_PROD)
-      on delete restrict on update restrict;
-
-alter table TRANSACCION
-   add constraint FK_TRANSACC_REFERENCE_PERSONA foreign key (ID_PERSONA)
-      references PERSONA (ID_PERSONA)
-      on delete restrict on update restrict;
-
-alter table TRAN_ESTADO
-   add constraint FK_TRAN_EST_REFERENCE_TRANSACC foreign key (NRO_TRANSAC)
-      references TRANSACCION (NRO_TRANSAC)
-      on delete restrict on update restrict;
-
-alter table TRAN_LINEA
-   add constraint FK_TRAN_LIN_REFERENCE_TRANSACC foreign key (NRO_TRANSAC)
-      references TRANSACCION (NRO_TRANSAC)
-      on delete restrict on update restrict;
-
-alter table TRAN_LINEA
-   add constraint FK_TRAN_LIN_REFERENCE_PRODUCTO foreign key (ID_PRODUCTO)
-      references PRODUCTO (ID_PRODUCTO)
       on delete restrict on update restrict;
