d2fa8fa98f8d34f2baccb58fecf86f251a8aa5b6
fedeolivera1 <fedeolivera1@gmail.com>
2017-09-21 21:43:54 -0300

    cmt [21/09] - 3
    Se mejora el script de unidad ya que faltaban params. Se modif el nombre
    de script que agrega usuario admin.

--- unidad.sql
+++ unidad.sql
@@ -1,5 +1,5 @@
-﻿INSERT INTO unidad (nombre) VALUES ('Unidad');
-INSERT INTO unidad (nombre) VALUES ('Kg');
-INSERT INTO unidad (nombre) VALUES ('Gr');
-INSERT INTO unidad (nombre) VALUES ('L');
-INSERT INTO unidad (nombre) VALUES ('Ml');
+﻿INSERT INTO unidad (nombre, sinc, activo) VALUES ('Unidad', 'N', 1);
+INSERT INTO unidad (nombre, sinc, activo) VALUES ('Kg', 'N', 1);
+INSERT INTO unidad (nombre, sinc, activo) VALUES ('Gr', 'N', 1);
+INSERT INTO unidad (nombre, sinc, activo) VALUES ('L', 'N', 1);
+INSERT INTO unidad (nombre, sinc, activo) VALUES ('Ml', 'N', 1);
